2f1a4f7c249ee73e6a5ca6535319eb3d5bf7b066
Tejas Shah <tejas.urwelcome@gmail.com>
2016-05-13 12:16:37 +0530

    Add MarsGrid class

---
+++ mars_grid.rb
@@ -0,0 +1,19 @@
+# Class Planet
+class MarsGrid
+  # Constructor
+  def initialize(top_right_x = 0, top_right_y = 0)
+    @top_right_x = top_right_x
+    @top_right_y = top_right_y
+  end
+
+  # Validates if rover coordinates/direction are valid within grid config
+  def valid?(rover_x, rover_y, rover_face)
+    if rover_x.between?(0, @top_right_x) &&
+       rover_y.between?(0, @top_right_y) &&
+       %w(N S E W).include?(rover_face)
+      return true
+    else
+      false
+    end
+  end
+end
