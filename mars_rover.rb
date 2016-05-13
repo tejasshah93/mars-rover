d2dc5c43f377ad9d4ebae98df7e3e3242699092a
Tejas Shah <tejas.urwelcome@gmail.com>
2016-05-13 12:16:50 +0530

    Add MarsRover class

---
+++ mars_rover.rb
@@ -0,0 +1,32 @@
+require_relative 'direction'
+
+# Class MarsRover
+class MarsRover
+  attr_accessor :id
+
+  # Constructor
+  def initialize(index, x, y, face)
+    @id = index
+    @dir = Direction.new(x, y, face)
+  end
+
+  # Prints mars_rover current location
+  def print_current_location
+    puts [@dir.x_value, @dir.y_value, @dir.face].join(', ')
+  end
+
+  # Executes move on 'M' instruction
+  def move
+    @dir.move_forward
+  end
+
+  # Executes turn_right on 'R' instruction
+  def rotate_right
+    @dir.turn_right
+  end
+
+  # Executes turn_left on 'L' instruction
+  def rotate_left
+    @dir.turn_left
+  end
+end
